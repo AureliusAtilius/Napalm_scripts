ed11023e4f8726edcf8e63793dbd1f535ebba855
Josh <j.j.h.lindner@gmail.com>
2021-06-28 19:56:00 -0700

    Create napalmbgp2.py
    Update README.md

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Use napalm to open an ssh session to s1 and print device facts.
 
 napalmbgp1.py
 Use napalm to return BGP neighbors.
+
+napalmbgp2.py
+Iterate over a list of router IPs and use Napalm to connect and retrieve BGP neighbor information.
---
+++ napalmbgp2.py
@@ -0,0 +1,18 @@
+import json
+from napalm import get_network_driver
+
+# List of router IP addresses
+bgp_list = ['192.168.255.70', '1923.168.255.1']
+
+# Iterate over router IP addresses
+for ip_address in bgp_list:
+        print("Connecting to "+str(ip_address))
+        # Connect to router
+        driver = get_network_driver('ios')
+        iosvrouter = driver(ip_address, 'josh', 'cisco')
+        iosvrouter.open()
+        # Retrieve and print BGP neighbor information
+        bgp_neighbors = iosvrouter.get_bgp_neighbors()
+        print (json.dumps(bgp_neighbors, indent=4))
+
+        iosvrouter.close()
